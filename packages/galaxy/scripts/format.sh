442013cba3423efd25afa62e491980ca44aff455
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-05-14 16:37:54 +0600

    Create format.sh
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ packages/galaxy/scripts/format.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# The source file is just YAML. We transform it to JSON to have both formats available.
+
+pnpm dlx @readyapi/cli format ./dist/3.1.yaml --output ./dist/3.1.json
+pnpm dlx @readyapi/cli format ./dist/latest.yaml --output ./dist/latest.json
