f67ded256695af550655946417a5d49049bffa77
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-05-21 12:44:19 +0600

    Create index.test.ts
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ packages/express-api-reference/test/index.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest'
+
+import { ApiReference } from '../src'
+
+describe('ApiReference', () => {
+  const url = 'https://cdn.jsdelivr.net/npm/@readyapi/galaxy/dist/latest.yaml'
+
+  it('renders the given spec URL', () => {
+    expect(ApiReference({ spec: { url } }).toString()).toContain(
+      `https://cdn.jsdelivr.net/npm/@readyapi/galaxy/dist/latest.yaml`,
+    )
+  })
+
+  it('renders the given spec URL with custom cdn', () => {
+    expect(
+      ApiReference({
+        spec: {
+          url,
+          cdn: 'https://fastly.jsdelivr.net/npm/@readyapi/api-reference',
+        },
+      }).toString(),
+    ).toContain(`https://cdn.jsdelivr.net/npm/@readyapi/galaxy/dist/latest.yaml`)
+  })
+})
